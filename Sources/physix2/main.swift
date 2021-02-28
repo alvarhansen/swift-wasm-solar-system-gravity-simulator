343e2f552c1b662f3d8198092e38c16d79d924bc
Alvar Hansen <alluxxii@gmail.com>
2021-02-28 15:34:06 +0200

    moon

--- Sources/physix2/main.swift
+++ Sources/physix2/main.swift
@@ -36,14 +36,14 @@ struct Planet {
     var velocity: Vector2
     var drawRadiusMultiplier: Double = 1
 
-    static let earth = Planet(
-        id: .init(value: "Earth"),
-        color: .init(value: "#00F"),
-        origin: Point(x: 1.495978707 * pow(10, 11), y: 0),
-        radius: 6.3781 * pow(10, 6),
-        mass: 5.972 * pow(10, 24),
-        velocity: Vector2(x: 0, y: 29.29 * pow(10, 3)),
-        drawRadiusMultiplier: 10
+
+    static let sun = Planet(
+        id: .init(value: "Sun"),
+        color: .init(value: "#FF0"),
+        origin: Point(x: 0, y: 0),
+        radius: 696.342 * pow(10, 6),
+        mass: 1.9885 * pow(10, 30),
+        velocity: .zero
     )
 
     static let venus = Planet(
@@ -56,14 +56,36 @@ struct Planet {
         drawRadiusMultiplier: 10
     )
 
-    static let sun = Planet(
-        id: .init(value: "Sun"),
-        color: .init(value: "#FF0"),
-        origin: Point(x: 0, y: 0),
-        radius: 696.342 * pow(10, 6),
-        mass: 1.9885 * pow(10, 30),
-        velocity: .zero
+    static let earth = Planet(
+        id: .init(value: "Earth"),
+        color: .init(value: "#00F"),
+        origin: Point(x: 1.495978707 * pow(10, 11), y: 0),
+        radius: 6.3781 * pow(10, 6),
+        mass: 5.972 * pow(10, 24),
+        velocity: Vector2(x: 0, y: 29.29 * pow(10, 3)),
+        drawRadiusMultiplier: 10
     )
+
+    static let earthMoon = Planet(
+        id: .init(value: "Earth Moon"),
+        color: .init(value: "#DDD"),
+        origin: Point(x: 1.495978707 * pow(10, 11) + 362.600 * pow(10, 6), y: 0),
+        radius: 1.737 * pow(10, 6),
+        mass: 7.342 * pow(10, 22),
+        velocity: Vector2(x: 0, y: 29.29 * pow(10, 3) + 1.002 * pow(10, 3)),
+        drawRadiusMultiplier: 10
+    )
+
+    static let mars = Planet(
+        id: .init(value: "Mars"),
+        color: .init(value: "#F33"),
+        origin: Point(x: 2.492 * pow(10, 11), y: 0),
+        radius: 3.389 * pow(10, 6),
+        mass: 6.4171 * pow(10, 23),
+        velocity: Vector2(x: 0, y: 24.007 * pow(10, 3)),
+        drawRadiusMultiplier: 10
+    )
+
 }
 
 extension Array {
@@ -216,7 +238,9 @@ let app = App(canvas: zoomCanvas)
 app.planets = [
     .sun,
     .venus,
-    .earth
+    .earth,
+    .earthMoon,
+    .mars
 ]
 
 var gen = SystemRandomNumberGenerator()
@@ -236,4 +260,7 @@ let toggle = JSClosure { _ in
 body.onclick = .object(toggle)
 
 app.start()
-
+//app.iterate()
+//app.iterate()
+//app.setTransform()
+//app.drawPlanets()
