7d9f0582cd12b126e90da83d484b0065ea1f1d3b
Alvar Hansen <alluxxii@gmail.com>
2021-03-06 10:53:55 +0200

    Add planets

--- Sources/physix2/Planet.swift
+++ Sources/physix2/Planet.swift
@@ -21,7 +21,17 @@ struct Planet {
         radius: 696.342 * pow(10, 6),
         mass: 1.9885 * pow(10, 30),
         velocity: .zero,
-        drawRadiusMultiplier: 0.1
+        drawRadiusMultiplier: 0.05
+    )
+
+    static let mercury = Planet(
+        id: .init(value: "Mercury"),
+        color: .init(value: "#828282"),
+        origin: Point(x: 69.8169 * pow(10, 9), y: 0),
+        radius: 2.4397 * pow(10, 3),
+        mass: 3.3011 * pow(10, 23),
+        velocity: Vector2(x: 0, y: 47.362 * pow(10, 3)),
+        drawRadiusMultiplier: 1000
     )
 
     static let venus = Planet(
@@ -30,7 +40,8 @@ struct Planet {
         origin: Point(x: 108.208 * pow(10, 9), y: 0),
         radius: 6.051 * pow(10, 6),
         mass: 4.8675 * pow(10, 24),
-        velocity: Vector2(x: 0, y: 35.02 * pow(10, 3))
+        velocity: Vector2(x: 0, y: 35.02 * pow(10, 3)),
+        drawRadiusMultiplier: 0.5
     )
 
     static let earth = Planet(
@@ -60,14 +71,27 @@ struct Planet {
         velocity: Vector2(x: 0, y: 24.007 * pow(10, 3))
     )
 
+
+    static let jupiter = Planet(
+        id: .init(value: "Jupiter"),
+        color: .init(value: "rgb(192,130,65)"),
+        origin: Point(x: 816.62 * pow(10, 9), y: 0),
+        radius: 69.911 * pow(10, 6),
+        mass: 1.8982 * pow(10, 27),
+        velocity: Vector2(x: 0, y: 24.007 * pow(10, 3)),
+        drawRadiusMultiplier: 0.1
+    )
+
 }
 
 extension Array where Element == Planet {
     static var sol: [Planet] = [
         .sun,
+        .mercury,
         .venus,
         .mars,
         .earth,
-        .earthMoon
+        .earthMoon,
+//        .jupiter
     ]
 }
